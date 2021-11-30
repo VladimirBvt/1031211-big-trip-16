f883aca8598d2af0b4df954ae9b6d40938b066a6
Vladimir_Bavtenko <batonich84@mail.ru>
2021-11-30 17:16:14 +0300

    Добавляет модули Сортировка;

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createSiteMenuTemplate } from './view/site-menu-view.js';
 import { renderTemplate, RenderPosition } from './render.js';
 import { createFilterTemplate } from './view/filter-view.js';
+import { createSortingTemplate } from './view/sorting-view.js';
+import { createFormOfCreation } from './view/form-of-creation-view.js';
+import { createEditForm } from './view/edit-form-view.js';
+import { createTripPoint } from './view/trip-point-view.js';
 
 const siteTripMainElement = document.querySelector('.trip-main__trip-controls');
 const siteTripControlsNavigation = siteTripMainElement.querySelector(
@@ -12,3 +16,16 @@ renderTemplate(siteTripControlsNavigation, createSiteMenuTemplate(), RenderPosit
 const siteTripControlFilters = siteTripMainElement.querySelector('.trip-controls__filters');
 
 renderTemplate(siteTripControlFilters, createFilterTemplate(), RenderPosition.BEFOREEND);
+
+const siteMainElement = document.querySelector('.page-body__page-main');
+const siteTripEventsElement = siteMainElement.querySelector('.trip-events');
+
+renderTemplate(siteTripEventsElement, createSortingTemplate(), RenderPosition.BEFOREEND);
+
+renderTemplate(siteTripEventsElement, createFormOfCreation(), RenderPosition.BEFOREEND);
+
+const siteTripEventsList = document.querySelector('.trip-events__list');
+
+renderTemplate(siteTripEventsList, createEditForm(), RenderPosition.BEFOREEND);
+
+renderTemplate(siteTripEventsList, createTripPoint(), RenderPosition.BEFOREEND);
